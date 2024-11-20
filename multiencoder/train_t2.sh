fd9b98ed872683090d08fcf85b1e5e12a41815de
Sxzhhw <757821625@qq.com>
2024-11-20 20:58:01 +0800

    Update train_t2.sh

--- multiencoder/train_t2.sh
+++ multiencoder/train_t2.sh
@@ -18,8 +18,6 @@ do
     --per_device_train_batch_size 1 \
     --max_source_length 256 \
     --generation_max_len 256 \
-    --device cuda \
-    --n_gpu 2 \
     --val_max_target_length 256 \
     --overwrite_output_dir \
     --per_device_eval_batch_size 1 \
